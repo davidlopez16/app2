78d18c06c9f72c2997e6a699247969e79c85a164
isidro <lopez.portal16@gmail.com>
2017-07-25 16:14:09 -0400

    Revert "cargar proyectos"
    This reverts commit 658cc586a2876721b38d7e1ac6326aa1176f5d52.

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170722210914) do
+ActiveRecord::Schema.define(version: 20170725000719) do
 
   create_table "autors", force: true do |t|
     t.string   "nombre"
     t.string   "apellido"
     t.string   "foto"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "categoria", force: true do |t|
+    t.string   "carrera"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "categorias", force: true do |t|
+    t.string   "carrera"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
